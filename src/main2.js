13cd8bd7123533affbad1bc5e9aaefa078493f70
rafuri <tsukino153@gmail.com>
2017-10-08 09:31:25 +0700

    Update main2.js

--- src/main2.js
+++ src/main2.js
@@ -126,26 +126,26 @@ class LINE extends Command {
         let receiver = messages.to;
         let sender = messages.from;
         
-        this.command('Bot1', ['Bot1 hadir']);
+        this.command('bot2', ['Bot1 hadir']);
         this.command('kamu siapa', this.getProfile.bind(this));
-        this.command('Bot1 Status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
-        this.command(`1left ${payload}`, this.leftGroupByName.bind(this));
-        this.command('Bot1 speed', this.getSpeed.bind(this));
+        this.command('bot2 Status', `Your Status: ${JSON.stringify(this.stateStatus)}`);
+        this.command(`2left ${payload}`, this.leftGroupByName.bind(this));
+        this.command('bot2 speed', this.getSpeed.bind(this));
         this.command('kernel', this.checkKernel.bind(this));
         this.command(`kick ${payload}`, this.OnOff.bind(this));
         this.command(`cancel ${payload}`, this.OnOff.bind(this));
         this.command(`qrp ${payload}`, this.OnOff.bind(this));
-        this.command(`1dadah ${payload}`,this.kickAll.bind(this));
-        this.command(`1cancel ${payload}`, this.cancelMember.bind(this));
-        this.command(`bot1 set`,this.setReader.bind(this));
-        this.command(`bot1 cek`,this.rechecks.bind(this));
-        this.command(`bot1 clearall`,this.clearall.bind(this));
-        this.command('bot1 myid',`Your ID: ${messages.from}`)
+        this.command(`2dadah ${payload}`,this.kickAll.bind(this));
+        this.command(`2cancel ${payload}`, this.cancelMember.bind(this));
+        this.command(`bot2 set`,this.setReader.bind(this));
+        this.command(`bot2 cek`,this.rechecks.bind(this));
+        this.command(`bot2 clearall`,this.clearall.bind(this));
+        this.command('bot2 myid',`Your ID: ${messages.from}`)
         this.command(`Modyarr ${payload}`,this.checkIP.bind(this))
-        this.command(`bot1_ig ${payload}`,this.checkIG.bind(this))
-        this.command(`bot1_qr ${payload}`,this.qrOpenClose.bind(this))
-        this.command(`bot1_joinqr ${payload}`,this.joinQr.bind(this));
-        this.command(`bot1_spam ${payload}`,this.spamGroup.bind(this));
+        this.command(`bot2_ig ${payload}`,this.checkIG.bind(this))
+        this.command(`bot2_qr ${payload}`,this.qrOpenClose.bind(this))
+        this.command(`bot2_joinqr ${payload}`,this.joinQr.bind(this));
+        this.command(`bot2_spam ${payload}`,this.spamGroup.bind(this));
         this.command(`creator`,this.creator.bind(this));
 
         this.command(`bot1_pap ${payload}`,this.searchLocalImage.bind(this));
